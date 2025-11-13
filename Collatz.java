7167aeb07d9677a3b3171e7d11b9f25f6f98e024
David <david.20797@gmail.com>
2025-11-13 13:59:34 +0200

    final test?

--- Collatz.java
+++ Collatz.java
@@ -4,15 +4,16 @@ public class Collatz {
 
 		int seed = Integer.parseInt(args[0]); //non-negative integer input by user
 		char mode = args[1].charAt(0); //mode input by user
-		String output = "";
-		String str;
-		int steps;
-		int num;
+		String output = ""; // output string for verbose mode
+		String str; // string to store hailstone sequence
+		int steps; // number of steps in hailstone sequence
+		int num; // current number in hailstone sequence
 		for (int current = 1; current <= seed; current++) {	
 			num = current;
 			steps = 0;
 			str = "";
 			// compute hailstone sequence for current seed
+			// do while loop to ensure at least one iteration for num = 1
 			do{
 				steps++;
 				str += num + " ";
@@ -23,17 +24,18 @@ public class Collatz {
 					num += 1;
 				}
 			} while (num != 1);
-			
+			// append final 1 to sequence
 			if (num == 1) {
 				if (mode == 'v'){
 					output += str + "1 ("+ (steps + 1) +")\n";
 				}
 			}
 		}
-		// output results concise mode
+		// output results verbose mode
 		if (mode == 'v'){
 			System.out.print(output);
 		}
+		// output results final statement
 		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1."); 
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,17 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        // Get the Inputs
+        String[] timeInput = args[0].split(":");
+        int addTime = Integer.parseInt(args[1]);
+
+        // Set the params
+        int hours = Integer.parseInt(timeInput[0]);
+        int minutes = Integer.parseInt(timeInput[1]);
+        // Calculate the new time
+        int totalMinutes = hours * 60 + minutes + addTime;
+        // Format and print the new time
+        int newHours = (totalMinutes / 60) % 24;
+        int newMinutes = totalMinutes % 60;
+        System.out.printf("%02d:%02d\n", newHours, newMinutes);
     }
 }
